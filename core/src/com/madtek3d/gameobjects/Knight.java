99d9c880d4c756da02f1f7f0a07b1401e96d4cdd
Antonio <ajruiz.dev@gmail.com>
2015-05-24 17:02:47 +0200

    Initial commit

---
+++ core/src/com/madtek3d/gameobjects/Knight.java
@@ -0,0 +1,7 @@
+package com.madtek3d.gameobjects;
+
+/**
+ * Created by Antonio on 24/05/2015.
+ */
+public class Knight {
+}
---
+++ core/src/com/madtek3d/screens/GameScreen.java
@@ -0,0 +1,43 @@
+package com.madtek3d.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Antonio on 24/05/2015.
+ */
+public class GameScreen extends Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
